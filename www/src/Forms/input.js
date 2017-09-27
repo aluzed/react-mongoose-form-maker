336f746a3d3ba5196b41a9a2851628741127ea82
Alexandre Penombre <alexandre.penombre@gmail.com>
2017-09-28 00:07:06 +0200

    patching forEach

--- www/src/Forms/input.js
+++ www/src/Forms/input.js
@@ -29,58 +29,57 @@ class Input extends React.Component {
         d = {},
         currentVal = currentInput.value
 
-    this.rules.forEach(r => {
-      switch(r.type) {
-        case INPUT_RULES.MINLENGTH :
-          d = r.details
-          if(currentVal.length < d.value)
-            errorCounter++
-          break
-        case INPUT_RULES.MAXLENGTH :
-          d = r.details
-          currentVal = currentVal.substr(0, d.value)
-          break
-        case INPUT_RULES.ONLYNUMERIC :
-          // let reg = new RegExp('^[0-9]+$')
-          // let match = currentVal.match(reg)
-
-          if(isNaN(currentVal)) {
-            if(currentVal.length > 0) {
-              currentVal = currentVal.substr(0, currentVal.length-1)
+    if(!!this.rules.forEach) {
+      this.rules.forEach(r => {
+        switch(r.type) {
+          case INPUT_RULES.MINLENGTH :
+            d = r.details
+            if(currentVal.length < d.value)
+              errorCounter++
+            break
+          case INPUT_RULES.MAXLENGTH :
+            d = r.details
+            currentVal = currentVal.substr(0, d.value)
+            break
+          case INPUT_RULES.ONLYNUMERIC :
+            if(isNaN(currentVal)) {
+              if(currentVal.length > 0) {
+                currentVal = currentVal.substr(0, currentVal.length-1)
+              }
             }
-          }
-          break
-        case INPUT_RULES.BEETWEEN :
-          d = r.details
-          currentVal = parseFloat(currentVal, 10)
+            break
+          case INPUT_RULES.BEETWEEN :
+            d = r.details
+            currentVal = parseFloat(currentVal, 10)
 
-          // If the size of the string is lower than d.min or greater than d.max
-          if(!(currentVal < d.max) || !(currentVal > d.min))
-            errorCounter++
-          break
-        case INPUT_RULES.GREATERTHAN :
-          d = r.details
-          currentVal = parseFloat(currentVal, 10)
+            // If the size of the string is lower than d.min or greater than d.max
+            if(!(currentVal < d.max) || !(currentVal > d.min))
+              errorCounter++
+            break
+          case INPUT_RULES.GREATERTHAN :
+            d = r.details
+            currentVal = parseFloat(currentVal, 10)
 
-          if(currentVal < d.value)
-            errorCounter++
-          break
-        case INPUT_RULES.LOWERTHAN :
-          d = r.details
-          currentVal = parseFloat(currentVal, 10)
+            if(currentVal < d.value)
+              errorCounter++
+            break
+          case INPUT_RULES.LOWERTHAN :
+            d = r.details
+            currentVal = parseFloat(currentVal, 10)
 
-          if(currentVal > d.value)
-            errorCounter++
-          break
-        case INPUT_RULES.REQUIRED :
-          if(currentVal === "")
-            errorCounter++
-          break
+            if(currentVal > d.value)
+              errorCounter++
+            break
+          case INPUT_RULES.REQUIRED :
+            if(currentVal === "")
+              errorCounter++
+            break
 
-        default:
-          break
-      }
-    })
+          default:
+            break
+        }
+      })
+    }
 
     (errorCounter > 0) ? this.setState({value: currentVal, haveError: true}) : this.setState({value: currentVal, haveError: false})
   }
